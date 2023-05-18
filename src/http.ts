c20e76abfcbaaa31b1fe183ab4901196036c0e36
Leonardo Machado <leonardo.canto@edu.pucrs.br>
2023-05-18 00:10:53 -0300

    create connections

--- src/http.ts
+++ src/http.ts
@@ -13,19 +13,23 @@ app.set("views", path.join(__dirname, "..", "public"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
-app.get("/pages/client", (request, response) =>{
-    return response.render("html/client.html")
-})
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+app.get("/pages/admin", (request, response) => {
+  return response.render("html/admin.html");
+});
 
 const http = createServer(app); // Criando protocolo http
-const io = new Server(http); // Crinado protocolo WS
+const io = new Server(http); // Criando protocolo ws
 
-io.on("connection" , (socket: Socket) => {
-    //console.log("Se conectou", socket.id);
+io.on("connection", (socket: Socket) => {
+  // console.log("Se conectou", socket.id);
 });
 
 app.use(express.json());
 
 app.use(routes);
 
-export { http, io }
+export { http, io };
